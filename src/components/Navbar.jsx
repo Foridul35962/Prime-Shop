3f8f4ae3db42268b151744e450471793208a2e84
Foridul35962 <foridulislam35962@gmail.com>
2025-08-27 03:52:09 +0600

    adding location

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { FaLocationDot } from 'react-icons/fa6';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { FaCaretDown } from 'react-icons/fa';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import axios from 'axios';
 
 
 
 const Navbar = () => {
 
-  const [isLocation, setIsLocation] = useState(false);
+  const [location, setLocation] = useState('');
+  const getLocation = async () => {
+    navigator.geolocation.getCurrentPosition(async pos => {
+      const { latitude, longitude } = pos.coords;
+      const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+      try {
+        const allLocation = await axios.get(url);
+        setLocation(allLocation.data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    })
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, [])
 
   return (
     <div className='flex justify-between py-3 items-center'>
@@ -19,12 +38,17 @@ const Navbar = () => {
             <span className='text-2xl text-black'>rime Shop</span>
           </Link>
         </div>
-        <div className='flex'>
+        <div className='flex items-center gap-2'>
           <div>
             <FaLocationDot className='text-red-600 text-xl' />
           </div>
-          <div>
-            {isLocation ? <div></div> : <div>Add Address</div>}
+          <div className='flex items-center gap-1.5'>
+            {location ? <div>
+              <p>{location.locality}</p>
+              <p>{location.city}</p>
+            </div> : <div>Add Address</div>
+            }
+            <FaCaretDown className='cursor-pointer' onClick={handleLocation} />
           </div>
         </div>
       </div>
