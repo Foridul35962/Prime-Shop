b949e1aea08b67147fdeef286b378cbdd0b458c3
Foridul35962 <foridulislam35962@gmail.com>
2025-09-08 01:41:42 +0600

    adding error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Lottie from 'lottie-react'
+import Error404 from '../assets/Error 404.json'
 
 const Error = () => {
   return (
-    <div>404 Not Found</div>
+    <div className='h-100 lg:h-dvh'>
+      <Lottie animationData={Error404} className='h-100 lg:h-dvh'></Lottie>
+    </div>
   )
 }
 
